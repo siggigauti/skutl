89b8c82183e28c4d253833589c0fb7938335d62f
siggigauti <siggigauti@gmail.com>
2016-09-06 14:44:48 +0000

    initial commit
    Komin server, engar síður né html, set það upp eftir smá.

---
+++ src/main/java/com/HBV501G/skutl/AppConfig.java
@@ -0,0 +1,15 @@
+package com.HBV501G.skutl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+/**
+ * Created by Siggigauti on 06/09/2016.
+ */
+
+@EnableAutoConfiguration
+public class AppConfig {
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfig.class, args);
+    }
+}
